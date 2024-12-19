42bfd8cecc0b78f3974934e4489d4d389c1a1371
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-19 12:12:23 +0330

    onchange

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function changetext(){
-    document.getElementById('zeynab').innerHTML='من زینبم'
+function fun(){
+ var x=document.getElementById('select_test').value;
+ document.getElementById('show').innerHTML=x
 }
