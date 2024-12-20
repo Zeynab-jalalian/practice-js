34ede85a2dd28379ff72abc636d30e67f619c158
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-21 00:11:09 +0330

    onresize

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+window.onresize= function(){
+let w=document.getElementById('w')
+let h=document.getElementById('h')
 
+let w=window.innerWidth
+let h=window.innerHeight
+
+w.innerHTML=w1
+h.innerHTML=h1
+}
