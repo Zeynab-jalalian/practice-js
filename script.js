16e367dd205b5019876eed065189de4af0cab3d7
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-20 23:30:19 +0330

    finish

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-for(let i=0;i<=3;i++){
+for(let i=1;i<=3;i++){
     let add_tag=document.createElement('p');
     add_tag.setAttribute('id','zeynab' + i)
     add_tag.appendChild(document.createTextNode('hi' + i))
     document.body.appendChild(add_tag)
+   
 }
-let remove_tag=document.getElementById('zeynab0')
+let remove_tag=document.getElementById('zeynab' + 2,3)
+
 document.body.removeChild(remove_tag)
