6947607841b5db607d59dc935b444e331ded38e4
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-20 23:25:52 +0330

    removechild

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 for(let i=0;i<=3;i++){
-    let at=document.createElement('p')
-    
+    let add_tag=document.createElement('p');
+    add_tag.setAttribute('id','zeynab' + i)
+    add_tag.appendChild(document.createTextNode('hi' + i))
+    document.body.appendChild(add_tag)
 }
+let remove_tag=document.getElementById('zeynab0')
+document.body.removeChild(remove_tag)
