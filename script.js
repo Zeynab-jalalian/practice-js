fa7e268e697b9624fa6578a87540f577a463fd3d
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-20 23:13:44 +0330

    createelement,appendchild,createtextnode

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let x=0;
-document.getElementById('divvvv').addEventListener('scroll',function test(){
-    document.getElementById('count').innerHTML=x+=1;
-})
-
+let tag_new=document.createElement('p')
+document.body.appendChild(tag_new)
+tag_new.appendChild(document.createTextNode('hi'))
+.tag_new.setAttribute('id','zeynab')
