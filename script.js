d5642a8033d9b53e9d506d0b2271a5a74d8385e2
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-21 00:00:27 +0330

    console.log(window);

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-for(let i=1;i<=3;i++){
-    let add_tag=document.createElement('p');
-    add_tag.setAttribute('id','zeynab' + i)
-    add_tag.appendChild(document.createTextNode('hi' + i))
-    document.body.appendChild(add_tag)
-   
-}
-let remove_tag=document.getElementById('zeynab' + 2,3)
-
-document.body.removeChild(remove_tag)
+console.log(window);
