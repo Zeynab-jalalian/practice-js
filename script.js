bae9f014f8608b6d20ac87bfcaf0653d7d971975
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-21 11:22:33 +0330

    setInterVal

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-setTimeout
+let count=0;
+let R=setInterval(result , 1000)
+function show(){
+    count++
+    console.log(count);
+    
+}
+function result(){
+    show()
+    if(count==10){
+        console.log('finish')
+        clearInterval(R)
+    }
+}
