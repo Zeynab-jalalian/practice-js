ade9523b9741a864faae89b9e704725eff834d2c
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-21 11:50:30 +0330

    screen

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-console.log(screen.width);
-console.log(screen.height)
+let one=document.getElementById('one').innerHTML=window.innerWidth
+let two=document.getElementById('two').innerHTML=window.innerHeight
+let three=document.getElementById('three').innerHTML=screen.width
+let four=document.getElementById('four').innerHTML=screen.height
