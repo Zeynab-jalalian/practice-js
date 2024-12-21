6c13ade3fbe26818a4aec254e3e9a1d586a74620
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-21 11:52:10 +0330

    finishh

--- script.js
+++
@@ -1,4 +0,0 @@
-let one=document.getElementById('one').innerHTML=window.innerWidth
-let two=document.getElementById('one').innerHTML=window.innerHeight
-let three=document.getElementById('three').innerHTML=screen.width
-let four=document.getElementById('four').innerHTML=screen.height
